0ca8b4fac9d2265f1bdf9403d5f8bf559131ebde
somitmittal <somitmittal@gmail.com>
2025-04-18 18:18:43 +0530

    added cron parser changes

--- src/main/kotlin/com/cronparser/CronAbstractFactory.kt
+++
@@ -1,9 +0,0 @@
-package com.cronparser
-
-import com.cronparser.factories.CronFieldFactory
-
-object CronAbstractFactory {
-    fun get(fieldType: String): AbstractHandler {
-        return CronFieldFactory().getFieldHandler(fieldType)
-    }
-}
--- src/main/kotlin/com/cronparser/CronParser.kt
+++ src/main/kotlin/com/cronparser/CronParser.kt
@@ -1,31 +1,37 @@
 package com.cronparser
 
-import com.cronparser.handlers.*
+import com.cronparser.factories.CronFieldFactory
 
 const val CRON_EXPRESSION_MAX_LENGTH = 6
+val INDEX_TO_FIELD_MAPPING = mapOf(
+    0 to CronField.MINUTES,
+    1 to CronField.HOURS,
+    2 to CronField.DAY_OF_MONTH,
+    3 to CronField.MONTH,
+    4 to CronField.DAY_OF_WEEK,
+)
 
 class CronParser(private val responseBuilder: ResponseBuilder) {
     
     fun parse(cronExpression: String): Map<String, Any> {
         val parts = cronExpression.split(" ")
         
-        if (parts.size < CRON_EXPRESSION_MAX_LENGTH) {
-            throw InvalidCronStringException("Expected [minute] [hour] [dayOfMonth] [dayOfWeek] [command] but got: $cronExpression")
+        if (parts.size != CRON_EXPRESSION_MAX_LENGTH) {
+            throw InvalidCronStringException("Expected [minute] [hour] [dayOfMonth] [month] [dayOfWeek] [command] but got: $cronExpression")
         }
-        
-        val minute = MinuteHandler().parse(parts[0])
-        val hour = HourHandler().parse(parts[1])
-        val dayOfMonth = DayOfMonthHandler().parse(parts[2])
-        val month = MonthHandler().parse(parts[3])
-        val dayOfWeek = DayOfWeekHandler().parse(parts[4])
-        val command = parts.subList(5, parts.size).joinToString(" ")
-        
-        val entries = listOf(
-            ResponseEntry("minute", minute.map { it.toString() }),
-            ResponseEntry("hour", hour.map { it.toString() }),
-            ResponseEntry("day_of_month", dayOfMonth.map { it.toString() }),
-            ResponseEntry("month", MonthHandler().representation(month)),
-            ResponseEntry("day_of_week", DayOfWeekHandler().representation(dayOfWeek)),
+        val entries = mutableListOf<ResponseEntry>()
+        for (index in 0..parts.size-2) {
+            val field = INDEX_TO_FIELD_MAPPING.get(index)
+            if (field == null) {
+                logger.error {"Index: $index is not mapped in $INDEX_TO_FIELD_MAPPING"}
+                throw Exception("Some Error while processing cron expression")
+            }
+            val cronField = field.name
+            val output = CronFieldFactory().getFieldHandler(cronField).parse(parts[index])
+            entries.add(ResponseEntry(cronField, output.map {it.toString()}))
+        }
+        val command = parts.subList(CRON_EXPRESSION_MAX_LENGTH-1, parts.size).joinToString(" ")
+        entries.add(
             ResponseEntry("command", listOf(command))
         )
         
